9e06cf70da2667e06ea3401a5d4c1f61cd5e3c55
Ermias <129514606+Ermi9s@users.noreply.github.com>
2023-11-29 18:39:53 +0300

    Added solution for Smallest Even Multiple

---
+++ Python_track/week1/smallest-even-multiple.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def smallestEvenMultiple(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        i = 1
+        while True:
+            if i % 2 == 0 and i % n == 0:
+                return i
+            i += 1
+
+        
