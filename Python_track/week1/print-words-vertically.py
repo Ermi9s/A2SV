67594e9f0b04784cb58a5daebabba553ea1bd54d
Ermias <129514606+Ermi9s@users.noreply.github.com>
2023-12-03 22:36:34 +0300

    Added solution for Print Words Vertically

---
+++ Python_track/week1/print-words-vertically.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def printVertically(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        temp = s.split()
+        mx = len(max(temp, key=len))
+        mp = {}
+        ans = ["" for _ in range(mx)]
+
+        for i in range(mx):
+            mp[i] = []
+        
+        for word in temp:
+            for l in range(len(word)):
+                mp[l].append(word[l])
+                if l == len(word)-1:
+                    for fil in range(l+1,mx):
+                        mp[fil].append(" ")
+        
+        for i in mp.values():
+            while i[-1] == " ":
+                i.pop()
+        for i , l in enumerate(mp.values()):
+            for j in l:
+                ans[i] += j
+            
+        
+
+        
+        return ans
+        
+
+
+
+
+        return " "
+
+
+
+
+            
+
+
+        
