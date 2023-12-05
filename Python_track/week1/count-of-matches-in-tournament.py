3ffa4ce5f6123c9c48d7d41ffc9ee2f37270eb5e
Ermias <129514606+Ermi9s@users.noreply.github.com>
2023-12-05 15:06:21 +0300

    Added solution for Count of Matches in Tournament

---
+++ Python_track/week1/count-of-matches-in-tournament.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numberOfMatches(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n > 1:
+            count += n//2
+            n = n//2 + n%2
+            
+        return count
