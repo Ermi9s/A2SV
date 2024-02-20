c1680fe2f4ef1e488f5f12373859ba05d0675519
Ermias <129514606+Ermi9s@users.noreply.github.com>
2024-02-20 18:21:41 +0300

    Add solution for Find Valid Matrix Given Row and Column Sums

---
+++ comp_prog/new/leetcode/find-valid-matrix-given-row-and-column-sums.py
@@ -0,0 +1,14 @@
+class Solution:
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        grid =  [[0 for _ in range(len(colSum))] for _ in range(len(rowSum))]
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                grid[i][j] = min(rowSum[i] , colSum[j])
+                rowSum[i] -= grid[i][j]
+                colSum[j] -= grid[i][j]
+        
+        return grid
+
+
+        
