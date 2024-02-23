dc63d3905a7cb8e5fd2fb9b1bcde579b023ad3a8
Ermias <129514606+Ermi9s@users.noreply.github.com>
2024-02-23 17:10:17 +0300

    Added solution for Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

---
+++ leetcode/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -0,0 +1,46 @@
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        incque = deque()
+        decque = deque()
+
+        ans = 0
+        s = 0
+        for i in range(len(nums)):
+            while incque and nums[i] < incque[-1][1]:
+                incque.pop()
+            while decque and nums[i] > decque[-1][1]:
+                decque.pop()
+            
+            incque.append([i , nums[i]])
+            decque.append([i , nums[i]])
+            # print(decque)
+            # print(incque)
+
+            while incque and decque and (decque[0][1] - incque[0][1]) > limit:
+                print(s, i)
+                if s == decque[0][0]:
+                    decque.popleft()
+                if s == incque[0][0]:
+                    incque.popleft()
+
+                s += 1
+
+           
+            # print("-------")
+
+            # start = min(incque[0][0] , decque[0][0])
+
+            # end = max(incque[-1][0] , decque[-1][0])
+
+            ans = max(ans , (i - s + 1))
+
+            
+
+        return ans 
+    
+
+
+
+
+
+        
