17d5f9fe78376b6ba8b531fd1446f35e15ec6143
Ermias <129514606+Ermi9s@users.noreply.github.com>
2024-07-09 01:39:29 +0300

    Time: 687 ms (61.8%), Space: 60.7 MB (28.65%) - LeetHub

--- 1296-kth-ancestor-of-a-tree-node/1296-kth-ancestor-of-a-tree-node.py
+++ 1296-kth-ancestor-of-a-tree-node/1296-kth-ancestor-of-a-tree-node.py
@@ -1,40 +1,41 @@
 class TreeAncestor:
     def __init__(self, n: int, parent: List[int]):
-        self.LOG = 0
-        self.depth = [0 for _ in range(n)]
         graph = defaultdict(list)
-
         for i in range(1 , n):
             graph[parent[i]].append(i)
         
+        self.max_log = 0
+        while 1 << self.max_log <= n:
+            self.max_log += 1
+        
+        self.depth = [0 for _ in range(n)]
+        self.ancestor = [[-1 for _ in range(self.max_log)] for _ in range(n)]
+
         stk = [0]
         while stk:
             node = stk.pop()
+            
+            self.ancestor[node][0] = parent[node]
+            for level in range(1 , self.max_log):
+                temp = self.ancestor[node][level-1]
+                self.ancestor[node][level] = self.ancestor[temp][level-1]
+
             for nbr in graph[node]:
+                self.depth[nbr] = self.depth[node]+1
                 stk.append(nbr)
-                self.depth[nbr] = self.depth[node] + 1
         
-        while 1 << self.LOG <= n:
-            self.LOG += 1
-
-        self.pre = [[0 for _ in range(self.LOG)] for _ in range(n)]
-
-        for i in range(n):
-            self.pre[i][0] = parent[i]
-
-        for j in range(1 , self.LOG):
-            for i in range(n):
-                self.pre[i][j] = self.pre[ self.pre[i][j-1] ][j-1]
 
     def getKthAncestor(self, node: int, k: int) -> int:
-        if k > self.depth[node]:
+        if self.depth[node] < k:
             return -1
-
-        for i in range(self.LOG):
-            if 1 << i & k != 0:
-                node = self.pre[node][i] 
+        
+        for i in range(self.max_log):
+            if 1<<i & k:
+                node = self.ancestor[node][i]
         
         return node
+
+        
 
 
 # Your TreeAncestor object will be instantiated and called as such:
