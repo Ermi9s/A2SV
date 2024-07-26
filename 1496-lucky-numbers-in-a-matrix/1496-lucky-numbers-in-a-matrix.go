2b75fdf635a1acbedd9652fa56d36866277c7935
Ermias <129514606+Ermi9s@users.noreply.github.com>
2024-07-26 17:23:40 +0300

    Time: 14 ms (11.07%), Space: 5.8 MB (9.92%) - LeetHub

---
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.go
@@ -0,0 +1,40 @@
+func min(arr []int) int{
+    mini := 1000000;
+    for i := range len(arr) {
+        if arr[i] < mini {
+            mini = arr[i];
+        }
+    }
+    return mini;
+}
+
+
+func luckyNumbers (matrix [][]int) []int {
+    row_ch := make(map[int]int)
+    col_ch := make(map[int]int)
+    
+    for i := range len(matrix) {
+        row_ch[i] = min(matrix[i]);
+    }
+
+    for j := 0; j < len(matrix[0]); j++ {
+        maxi := 0
+        for i := 0; i < len(matrix); i++ {
+            if matrix[i][j] > maxi {
+                maxi = matrix[i][j];
+            }
+        }
+        col_ch[j] = maxi;
+    }
+
+    ans := make([]int , 0);
+    for i := range len(matrix) {
+        for j := range len(matrix[0]) {
+            if col_ch[j] == matrix[i][j] && row_ch[i]== matrix[i][j] {
+                ans = append(ans , matrix[i][j])
+            }
+        }
+    }
+    return ans;
+    
+}
