e927027809bcfc986a5dc707986a8d25df7d1b75
Ermias <129514606+Ermi9s@users.noreply.github.com>
2024-09-16 15:21:00 +0300

    Time: 2 ms (49.2%), Space: 7.5 MB (69.75%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int memo[46];
+    int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+
+        if(memo[n]){
+            return memo[n];
+        }
+        memo[n] = climbStairs(n-1) + climbStairs(n-2);
+
+        return memo[n];
+    }
+};
