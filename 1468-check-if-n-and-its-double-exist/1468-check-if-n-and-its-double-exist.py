ce7bbe9b1a5a013df7fdef8d622fe90f39c216d9
Ermias <129514606+Ermi9s@users.noreply.github.com>
2024-12-01 13:34:23 +0300

    Time: 2 ms (60.19%), Space: 17.4 MB (5.25%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -4,16 +4,10 @@ class Solution:
         mp = set()
 
         for i in range(len(arr)):
-            if arr[i]/2 in mp:
+            if arr[i]/2 in mp or arr[i]*2 in mp:
                 return True
             mp.add(arr[i])
-    
-        mp = set()
 
-        for i in range(len(arr)-1 , -1 , -1):
-            if arr[i]/2 in mp:
-                return True
-            mp.add(arr[i])
         
         return False
         
