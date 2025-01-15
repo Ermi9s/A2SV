749a7b2c861e9cc59143a84fd43289ce8a438d99
Ermias <129514606+Ermi9s@users.noreply.github.com>
2025-01-15 15:20:42 +0300

    Time: 0 ms (100%), Space: 17.8 MB (41.6%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        count = 0
+        x = 0
+
+        for i in range(32):
+            if (1 << i) & num2:
+                count += 1
+        c = 33
+        while count and c >= 0:
+            if (1 << c) & num1:
+                x |= (1 << c)
+                count -= 1
+            c -= 1
+        c = 0
+        print(count)
+        while count:
+            if not ((1 << c) & num1):
+                x |= (1 << c)
+                count -= 1
+            c += 1
+
+        return x 
