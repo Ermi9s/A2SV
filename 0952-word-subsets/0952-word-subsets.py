a549d778de55f0ac7000c0ea0e987e47219f7cdb
Ermias <129514606+Ermi9s@users.noreply.github.com>
2025-01-16 14:27:28 +0300

    Time: 205 ms (87.04%), Space: 21 MB (93.28%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.py
@@ -0,0 +1,36 @@
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        max_occ = [0] * 26
+
+        def check(word):
+            temp = [0] * 26
+
+            for ch in word:
+                ind = ord(ch) - ord('a')
+                temp[ind] += 1
+                max_occ[ind] = max(max_occ[ind] , temp[ind])
+        
+        for word in words2:
+            check(word)
+        
+        print(max_occ)
+        def validate(word):
+            temp = [0] * 26
+
+            for ch in word:
+                ind = ord(ch) - ord('a')
+                temp[ind] += 1
+
+            for i in range(26):
+                if max_occ[i] > temp[i]:
+                    return False
+            
+            return True
+        
+        ans = []
+
+        for word in words1:
+            if validate(word):
+                ans.append(word)
+        
+        return ans
