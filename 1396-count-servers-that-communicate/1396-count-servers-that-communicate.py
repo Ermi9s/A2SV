95c1d3d084b266db2f6ac28a834b312e99f53643
Ermias <129514606+Ermi9s@users.noreply.github.com>
2025-01-24 00:32:10 +0300

    Time: 683 ms (5.02%), Space: 20.6 MB (7.17%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
@@ -1,35 +1,3 @@
-class DSU:
-    def __init__(self,size):
-        self.parent = {i : i for i in range(size+1)}
-        self.size = {i:1 for i in range(size+1)}
-    
-
-    def find(self,node):
-
-        while self.parent[node] != node:
-            self.parent[node] = self.parent[self.parent[node]]
-            node = self.parent[node]
-        
-        return node
-    
-
-    def union(self, a , b):
-        pa = self.find(a)
-        pb = self.find(b)
-
-        if pa == pb:
-            return 
-        
-        if self.size[pa] < self.size[pb]:
-            pa,pb = pb,pa
-        
-        self.parent[pb] = pa
-        self.size[pa] += self.size[pb]
-    
-    def isConnected(self, a , b):
-        return self.find(a) == self.find(b)
-
-
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         n = len(grid)
@@ -42,19 +10,19 @@ class Solution:
             
             return False
         
-        un = DSU(n+1)
-    
         score = {}
+        touched = defaultdict(set)
         ans = 0
         for i in range(n):
             score[i] = sum(grid[i])
-        
+            touched[i].add(i)
+
             for j in range(i-1 , -1 , -1):
-                if isConnected(i , j) and not un.isConnected(i , j):
+                if isConnected(i , j) and j not in touched[i]:
                     score[i] += score[j]
+                    touched[i] = touched[i].union(touched[j])
                     if score[j] > 1:
                         ans -= score[j]
-                    un.union(i , j)
                
             if score[i] > 1:
                 ans += score[i]
